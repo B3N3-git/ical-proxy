e5bcb7cf6a219077fd77879c32150be3eac8cfa0
Benedikt <ww01@benedikt-hirsch.de>
2026-06-06 19:53:08 +0200

    few ajustments

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # ical-proxy
 
-## A simple proxy for ics subscriptions. I created it for adding Icloud Calendars to my Nextcloud instance.
+## A simple proxy for ics subscriptions. I created it for adding Icloud calendar subscriptions to my Nextcloud instance.
 
 ### Fetch ics file
 
-http\<s\>://\<ip-address\>:\<port\>/ical_url?url=\<your_url_to_the_ics_file\>
+http\<s\>://\<ip-address\>:\<port\>/ical-proxy?url=\<your_url_to_the_ics_file\>
+
+### Docker
 
 You have to build your own docker image because I am to lazy to create a Docker account.
 
-Just execute this command: docker build -t ical-proxy .
+Just execute this command: **docker build -t ical-proxy .** after you copied this git repo.
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, Response
 import cal_parser
 import logging
 import sys
+import re
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,7 +15,7 @@ logging.basicConfig(
 app = Flask(__name__)
 
 
-@app.route('/ical_url', methods=['GET'])
+@app.route('/ical-proxy', methods=['GET'])
 def get_ical():
     if request.headers.getlist("X-Forwarded-For"):
         client_ip = request.headers.getlist("X-Forwarded-For")[0]
@@ -29,16 +31,20 @@ def get_ical():
 
     try:
         ics_content, cal_name = cal_parser.generate_ics(url)
+        cal_name = clean_filename(cal_name)
         logging.info(f"Response for ICAL calendar {cal_name} is sent")
         return Response(
             ics_content,
             mimetype='text/calendar',
-            headers={'Content-Disposition': 'attachment; filename=calendar.ics'}
+            headers={'Content-Disposition': f"attachment; filename={cal_name}.ics"}
         )
     except Exception as e:
         logging.error(f"Error while generating ics: {str(e)}")
         return f"Error while generating ics: {str(e)}", 500
 
+def clean_filename(filename):
+    filename = filename.replace(' ', '_')
+    return re.sub(r'[^\w\s-]', '', filename).strip()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
